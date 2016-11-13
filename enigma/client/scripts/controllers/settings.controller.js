424c628afa43206040a05b91d7efd1c7f476da61
ngorham <ngorham2@gmail.com>
2016-11-12 17:27:26 -0800

    Settings update
    settings.controller.js - logout functionality added, profile
    functionality added
    settings.html - logout functionality added, profile functionality added

--- enigma/client/scripts/controllers/settings.controller.js
+++ enigma/client/scripts/controllers/settings.controller.js
@@ -2,7 +2,22 @@ import { Meteor } from 'meteor/meteor';
 import { Controller } from 'angular-ecmascript/module-helpers';
 
 export default class SettingsCtrl extends Controller {
+    logout(){
+        Meteor.logout((err) => {
+            if (err) return this.handleError(err);
+            this.$state.go('login');
+        })
+    }
+    profile(){ this.$state.go('profile'); }
+    handleError (err) {
+        this.$log.error('Settings modification error', err);
 
+        this.$ionicPopup.alert({
+            title: err.reason || 'Settings modification failed',
+            template: 'Please try again',
+            okType: 'button-positive button-clear'
+        });
+    }
 }
 
 SettingsCtrl.$inject = ['$state', '$ionicPopup', '$log'];
