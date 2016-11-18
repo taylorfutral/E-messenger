55ed0407c2ca213f7049c5294a0fe22800663801
Eddie Schubert <schuberteddie@gmail.com>
2016-11-18 01:16:50 +0000

    Completing user search utility

--- enigma/client/scripts/controllers/search.controller.js
+++ enigma/client/scripts/controllers/search.controller.js
@@ -7,13 +7,24 @@ export default class SearchCtrl extends Controller {
     constructor($scope) {
         super(...arguments);
         this.results = [];
+        this.$scope = $scope;
     }
     findUsers() {
         if (_.isEmpty(this.query)) return;
         
-        var response = this.callMethod('userSearch', this.query);
-        this.results = response;
-        console.log(this.results);
+        var self = this;
+        
+        Meteor.call('userSearch', this.query, function(error, response) {
+            if (error) {
+                throw "ERROR: Search utility cannot access server"
+            } else {
+                self.results.length = 0; //clear array without re-assignment
+                for (row in response) {
+                    self.results.push(response[row]);
+                }
+            }
+            self.$scope.$apply(); //force view update
+        });
     }
 }
 
--- enigma/lib/methods.js
+++ enigma/lib/methods.js
@@ -13,19 +13,15 @@ Meteor.methods({
   userSearch(queryString) {
     //simple search for users, currently searches usernames
     var query = {
-      $where: function() {
-        return (this.username.includes(queryString));
-      }     
+      $where: "this.username.includes('"+queryString+"')"
     };
-    var projection = { //limits what the search will return
+    var fields = { //limits what the search will return
       username: 1,
       emails: 1,
-      createdAt: 0,
-      profile: 1,
-      services: 0
+      profile: 1
     };
     
-    var results = Meteor.users.find(/*query, projection*/); 
+    var results = Meteor.users.find(query, {'fields': fields});
     return results.fetch();
   }
 });
