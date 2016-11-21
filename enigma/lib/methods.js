803945f20bc6b6b0de95db466f8221ab37602294
taylorfutral <taylorfutral@eduroam-169-233-233-29.ucsc.edu>
2016-11-21 15:18:03 -0800

    Added packages and profile pic set up

--- enigma/lib/methods.js
+++ enigma/lib/methods.js
@@ -9,5 +9,18 @@ Meteor.methods({
     Chats.update(message.chatId, { $set: { lastMessage: message } });
 
     return messageId;
+  },
+  
+
+  updatePicture(data) {
+      if(!this.userId){
+	  throw new Meteor.Error('not-logged-in',
+				 'Must be logged in to update his picture.');
+      }
+
+      check(data, String);
+
+      return Meteor.users.update(this.userId, { $set: { 'profile.picture': data } });
   }
+
 });
